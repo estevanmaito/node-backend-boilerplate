d9d7c0834fb6b153f040cdf0c1b2d0c60d72b40b
Estevan Maito <ejmaito@gmail.com>
2019-03-04 10:02:50 -0300

    add register error handler

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,6 +4,15 @@ const resolvers = {
   },
   Mutation: {
     register: (_, { email, password }, { models }) => {
+      const userExists = models.User.findOne({ email });
+
+      if (userExists) {
+        return {
+          field: "email",
+          message: "Email already in use. Try to login instead."
+        };
+      }
+
       const user = new models.User({
         email,
         password
@@ -13,7 +22,7 @@ const resolvers = {
         if (err) throw new Error("Cannot save user!");
       });
 
-      return true;
+      return null;
     }
   }
 };
